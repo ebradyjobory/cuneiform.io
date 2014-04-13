2911932992639f4a4c4554fae33d06abe9c28cf4
Essam Joubori <essam.joubori@gmail.com>
2014-04-13 17:29:47 -0500

    Fixed logo position and background image

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Cuneiform::Application.routes.draw do
-  get "pages/index"
+
   root "pages#index"
-  
+
   match '/', to: 'pages#index', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
